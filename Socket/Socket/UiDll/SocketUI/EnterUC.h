a1c3407613d2fe074d7f77e51167d8a88a5fef04
Home 02 <mohammaderahimi80@gmail.com>
2021-07-15 18:59:26 +0430

    سرور

---
+++ Socket/Socket/UiDll/SocketUI/EnterUC.h
@@ -0,0 +1,78 @@
+#pragma once
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+
+namespace SocketUI {
+
+	/// <summary>
+	/// Summary for EnterUC
+	/// </summary>
+	public ref class EnterUC : public System::Windows::Forms::UserControl
+	{
+	public:
+		EnterUC(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~EnterUC()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(224, 178);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Sighin";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// EnterUC
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->Controls->Add(this->button1);
+			this->Name = L"EnterUC";
+			this->Size = System::Drawing::Size(523, 378);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
--- Socket/Socket/UiDll/SocketUI/Sever Winform.cpp
+++
@@ -1,2 +0,0 @@
-#include "Sever Winform.h"
-
