b2f5848bce74ea1a99131a71517d27397d5b7c0d
Home 02 <mohammaderahimi80@gmail.com>
2021-07-17 13:05:19 +0430

    start kar nemikone

--- Socket/Socket/UiDll/SocketUI/EnterUC.h
+++ Socket/Socket/UiDll/SocketUI/EnterUC.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 using namespace System;
 using namespace System::ComponentModel;
@@ -35,7 +35,14 @@ namespace SocketUI {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox2;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button2;
+
 	protected:
 
 	private:
@@ -51,26 +58,113 @@ namespace SocketUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::Color::Lime;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Britannic Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox2->Location = System::Drawing::Point(196, 157);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->PasswordChar = '.';
+			this->textBox2->Size = System::Drawing::Size(218, 27);
+			this->textBox2->TabIndex = 19;
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::Color::Lime;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(196, 64);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(218, 28);
+			this->textBox1->TabIndex = 18;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(224, 178);
+			this->button1->BackColor = System::Drawing::Color::Lime;
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"B Nazanin", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->button1->ForeColor = System::Drawing::Color::Black;
+			this->button1->Location = System::Drawing::Point(197, 220);
+			this->button1->Margin = System::Windows::Forms::Padding(2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 3;
-			this->button1->Text = L"Sighin";
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Size = System::Drawing::Size(83, 34);
+			this->button1->TabIndex = 17;
+			this->button1->Text = L"ورود";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label3->Font = (gcnew System::Drawing::Font(L"B Nazanin", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->label3->Location = System::Drawing::Point(193, 131);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(46, 24);
+			this->label3->TabIndex = 16;
+			this->label3->Text = L"گذرواژه";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label4->Font = (gcnew System::Drawing::Font(L"B Nazanin", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->label4->Location = System::Drawing::Point(193, 25);
+			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(61, 24);
+			this->label4->TabIndex = 15;
+			this->label4->Text = L"نام کاربری";
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Lime;
+			this->button2->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"B Nazanin", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->button2->ForeColor = System::Drawing::Color::Black;
+			this->button2->Location = System::Drawing::Point(300, 220);
+			this->button2->Margin = System::Windows::Forms::Padding(2);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(114, 34);
+			this->button2->TabIndex = 20;
+			this->button2->Text = L"فراموشی رمز عبور";
+			this->button2->UseVisualStyleBackColor = false;
 			// 
 			// EnterUC
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::GhostWhite;
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label4);
 			this->Name = L"EnterUC";
-			this->Size = System::Drawing::Size(523, 378);
+			this->Size = System::Drawing::Size(600, 300);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
